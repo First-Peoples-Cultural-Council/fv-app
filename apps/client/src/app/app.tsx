8f6a92bd46dac037c0d13be453ef5e4dfb3fe238
Jonathan Aaron <jonathana@gurutechnologies.net>
2023-02-03 08:38:42 -0700

    feat: add Tailwindcss

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="client" />
-      <div />
+      <div>Hi</div>
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./apps/client/src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

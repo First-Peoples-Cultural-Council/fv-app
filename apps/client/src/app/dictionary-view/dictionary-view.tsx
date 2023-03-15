b25b060176f3c0122f89ff2453ca4125bc191a4e
Jonathan Aaron <jonathana@gurutechnologies.net>
2023-03-15 09:43:27 -0600

    fix: move dictionary content to the dictionary-view component

--- apps/client/src/app/dictionary-view/dictionary-view.tsx
+++ apps/client/src/app/dictionary-view/dictionary-view.tsx
@@ -1,12 +1,48 @@
-import styles from './dictionary-view.module.css';
+import { dataDict } from '../temp-word-list';
+import { Fragment } from 'react';
+import WordCardMobile from '../dictionary-page/word-card-mobile';
+import WordCardDesktop from '../dictionary-page/word-card-desktop';
 
 /* eslint-disable-next-line */
 export interface WordsViewProps {}
 
 export function DictionaryView(props: WordsViewProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WordsView!</h1>
+    <div>
+      {dataDict.map((term) => {
+        return (
+          <Fragment key={`${term.source}-${term.entryID}`}>
+            <WordCardMobile
+              word={term.word}
+              definition={term.definition}
+              audio={term.audio}
+              img={term.img}
+              source={term.source}
+              entryID={term.entryID}
+              theme={term.theme}
+              secondary_theme={term.secondary_theme}
+              optional={term.optional}
+              compare_form={term.compare_form}
+              sort_form={term.sort_form}
+              sorting_form={term.sorting_form}
+            />
+            <WordCardDesktop
+              word={term.word}
+              definition={term.definition}
+              audio={term.audio}
+              img={term.img}
+              source={term.source}
+              entryID={term.entryID}
+              theme={term.theme}
+              secondary_theme={term.secondary_theme}
+              optional={term.optional}
+              compare_form={term.compare_form}
+              sort_form={term.sort_form}
+              sorting_form={term.sorting_form}
+            />
+          </Fragment>
+        );
+      })}{' '}
     </div>
   );
 }

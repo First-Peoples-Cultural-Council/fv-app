33dec3931ddc507ed7ab88d4d8eb9f0d9a0502bd
Scott Morton <scottm@gurutechnologies.net>
2023-03-23 11:07:54 -0600

    Added in the content for the mobile layout of the Category page.

--- apps/client/src/app/category-view/category-view.tsx
+++ apps/client/src/app/category-view/category-view.tsx
@@ -54,69 +54,45 @@ export function CategoryView(props: CategoryViewProps) {
   }, [selected]);
 
   return (
-    <div className={styles['container']}>
-      <div className="flex flex-row">
-        <div className="min-w-400 mr-8">
-          <div>
-            <Link
-              to={`/categories/${primaryCategory.id}`}
+    <>
+      <div className="block sm:hidden">
+        <div className="flex flex-auto">
+          <Link
+            to={`/categories/${primaryCategory.id}`}
+            className={classNames(
+              'transition duration-500 ease-in-out rounded-lg pr-4 flex items-center cursor-pointer text-tertiaryB bg-gray-300 p-2 mt-2 hover:opacity-75'
+            )}
+          >
+            <i
               className={classNames(
-                'transition duration-500 ease-in-out rounded-lg pr-4 flex items-center cursor-pointer text-tertiaryB bg-gray-300 p-2 mt-2 hover:opacity-75'
+                primaryCategory.icon ?? 'fv-categories',
+                'text-3xl hover:opacity-75'
               )}
-            >
-              <i
+            />
+            <div className="inline-flex text-lg font-medium">
+              {primaryCategory.name}
+            </div>
+          </Link>
+          {subCategories.map((subCategory) => {
+            return (
+              <Link
+                to={`/categories/${subCategory.id}`}
                 className={classNames(
-                  primaryCategory.icon ?? 'fv-categories',
-                  'text-3xl hover:opacity-75'
+                  'mt-2 transition duration-500 ease-in-out ml-4 p-2 rounded-lg flex items-center cursor-pointer hover:opacity-75',
+                  {
+                    'bg-gray-300 rounded-md':
+                      subCategory.id === currentCategory.id,
+                  }
                 )}
-              />
-              <div className="inline-flex text-lg font-medium">{primaryCategory.name}</div>
-            </Link>
-
-            {subCategories.map((subCategory) => {
-              return (
-                <Link
-                  to={`/categories/${subCategory.id}`}
-                  className={classNames(
-                    'mt-2 transition duration-500 ease-in-out ml-4 lg:ml-8 pr-4 lg:px-0 rounded-lg flex items-center cursor-pointer hover:opacity-75',
-                    {
-                      'bg-gray-300 rounded-md':
-                        subCategory.id === currentCategory.id,
-                    }
-                  )}
-                >
-                  <div className="inline-flex text-lg font-medium pl-2 hover:opacity-75">{subCategory.name}</div>
-                </Link>
-              );
-            })}
-          </div>
-          <div className="border-gray-700 border-solid border w-full mt-5 mb-5" />
-          <div className="">
-            {dataCategories
-              .filter(
-                (category) => category.parent == null && category.id !== id
-              )
-              .map((category) => {
-                return (
-                  <Link
-                    to={`/categories/${category.id}`}
-                    className={classNames(
-                      'flex items-center cursor-pointer mb-6 hover:opacity-75'
-                    )}
-                  >
-                    <i
-                      className={classNames(
-                        category.icon ?? 'fv-categories',
-                        'text-3xl hover:opacity-75'
-                      )}
-                    />
-                    <div className="pt-2 text-lg">{category.name}</div>
-                  </Link>
-                );
-              })}
-          </div>
+              >
+                <div className="inline-flex text-lg font-medium pl-2 hover:opacity-75">
+                  {subCategory.name}
+                </div>
+              </Link>
+            );
+          })}
         </div>
-        <div className="">
+        <div>
           <MultiSwitch
             selected={selected}
             items={[
@@ -175,7 +151,137 @@ export function CategoryView(props: CategoryViewProps) {
             })}{' '}
         </div>
       </div>
-    </div>
+
+      <div className="flex flex-row hidden sm:block">
+        <div className={styles['container']}>
+          <div className="flex flex-row">
+            <div className="min-w-400 mr-8">
+              <div>
+                <Link
+                  to={`/categories/${primaryCategory.id}`}
+                  className={classNames(
+                    'transition duration-500 ease-in-out rounded-lg pr-4 flex items-center cursor-pointer text-tertiaryB bg-gray-300 p-2 mt-2 hover:opacity-75'
+                  )}
+                >
+                  <i
+                    className={classNames(
+                      primaryCategory.icon ?? 'fv-categories',
+                      'text-3xl hover:opacity-75'
+                    )}
+                  />
+                  <div className="inline-flex text-lg font-medium">
+                    {primaryCategory.name}
+                  </div>
+                </Link>
+
+                {subCategories.map((subCategory) => {
+                  return (
+                    <Link
+                      to={`/categories/${subCategory.id}`}
+                      className={classNames(
+                        'mt-2 transition duration-500 ease-in-out ml-4 lg:ml-8 pr-4 lg:px-0 rounded-lg flex items-center cursor-pointer hover:opacity-75',
+                        {
+                          'bg-gray-300 rounded-md':
+                            subCategory.id === currentCategory.id,
+                        }
+                      )}
+                    >
+                      <div className="inline-flex text-lg font-medium pl-2 hover:opacity-75">
+                        {subCategory.name}
+                      </div>
+                    </Link>
+                  );
+                })}
+              </div>
+              <div className="border-gray-700 border-solid border w-full mt-5 mb-5" />
+              <div className="">
+                {dataCategories
+                  .filter(
+                    (category) => category.parent == null && category.id !== id
+                  )
+                  .map((category) => {
+                    return (
+                      <Link
+                        to={`/categories/${category.id}`}
+                        className={classNames(
+                          'flex items-center cursor-pointer mb-6 hover:opacity-75'
+                        )}
+                      >
+                        <i
+                          className={classNames(
+                            category.icon ?? 'fv-categories',
+                            'text-3xl hover:opacity-75'
+                          )}
+                        />
+                        <div className="pt-2 text-lg">{category.name}</div>
+                      </Link>
+                    );
+                  })}
+              </div>
+            </div>
+
+            <div>
+              <MultiSwitch
+                selected={selected}
+                items={[
+                  { name: 'WORDS', icon: 'fv-words' },
+                  { name: 'PHRASES', icon: 'fv-quote-right' },
+                  { name: 'BOTH', icon: null },
+                ]}
+                onToggle={(index: number) => {
+                  setSelected(index);
+                }}
+              />
+              {data
+                .filter((term) => {
+                  if (currentCategory === primaryCategory) {
+                    return term.theme === primaryCategory.id;
+                  } else {
+                    return (
+                      term.theme === primaryCategory.id &&
+                      term.secondary_theme === currentCategory.id
+                    );
+                  }
+                })
+                .map((term) => {
+                  return (
+                    <Fragment key={`${term.source}-${term.entryID}`}>
+                      <WordCardMobile
+                        word={term.word}
+                        definition={term.definition}
+                        audio={term.audio}
+                        img={term.img}
+                        source={term.source}
+                        entryID={term.entryID}
+                        theme={term.theme}
+                        secondary_theme={term.secondary_theme}
+                        optional={term.optional}
+                        compare_form={term.compare_form}
+                        sort_form={term.sort_form}
+                        sorting_form={term.sorting_form}
+                      />
+                      <WordCardDesktop
+                        word={term.word}
+                        definition={term.definition}
+                        audio={term.audio}
+                        img={term.img}
+                        source={term.source}
+                        entryID={term.entryID}
+                        theme={term.theme}
+                        secondary_theme={term.secondary_theme}
+                        optional={term.optional}
+                        compare_form={term.compare_form}
+                        sort_form={term.sort_form}
+                        sorting_form={term.sorting_form}
+                      />
+                    </Fragment>
+                  );
+                })}{' '}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 

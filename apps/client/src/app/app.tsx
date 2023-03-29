d2b5a146894ac5d4b4273921987862b4daacacad
Scott Morton <scottm@gurutechnologies.net>
2023-03-29 13:24:56 -0600

    Removed the class on the app that were causing some issues with scrolling.

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 export function App() {
   return (
-    <div className="max-h-[100vh] overflow-hidden">
+    <div className="">
       <Header />
       <Outlet />
     </div>

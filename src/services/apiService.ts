a6c1556d81417f03556d2f28e7541b9143c8a9d6
Scott Morton <scottm@gurutechnologies.net>
2023-05-31 14:37:26 -0600

    Updated the api for word of the day to use api properties from the env file. Updated the readme about using the env file.

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 export interface WordOfDayDataResponse {
   dictionaryEntry: {
     id: string;
-  }
+  };
 }
 
 export const fetchWordOfDayData = async (): Promise<WordOfDayDataResponse> => {
   try {
     const response = await axios.get(
-      'https://fv-be-dev.eks.firstvoices.io/api/1.0/sites/mock-site/word-of-the-day/'
+      `${process.env.REACT_APP_API_URL}${process.env.REACT_APP_API_SITE}/word-of-the-day/`
     );
     return response.data[0] as WordOfDayDataResponse;
   } catch (error) {

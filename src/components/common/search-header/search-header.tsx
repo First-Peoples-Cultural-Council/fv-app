80fb3325e64ee33541288007b99aab088bb3e777
Jonathan Aaron <jonathana@gurutechnologies.net>
2023-06-24 05:46:43 -0600

    Minor format change

--- src/components/common/search-header/search-header.tsx
+++ src/components/common/search-header/search-header.tsx
@@ -91,7 +91,7 @@ export function SearchHeader({
           getResults(e?.target?.value);
         }}
         clickSearch={() => console.log(`search for ${searchValue}`)}
-        />
+      />
     </header>
   );
 }

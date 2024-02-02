229ea7af1dc26ac908b6ae0446de51ab6ef5ed5c
Rohit Sharma <rsharma@fpcc.ca>
2024-02-02 13:59:27 -0800

    Trigger the terraform init with creds

--- modules/terraform.tf
+++ modules/terraform.tf
@@ -1,13 +1,22 @@
+# Please do not change the contents of this file
+
 terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version  = "~> 5.0"
+        }
+    }
+
     backend "s3" {
-        encrypt = true
-        bucket  = ""
-        dynamodb_table = ""
-        key = ""
-        region = ""
+        encrypt = true # must always be encrypted
+        bucket  = "fpcc-tf-state-ca-central-1" # state bucket name - non sensitive
+        dynamodb_table = "fpcc-tf-state-lock-ca-central-1" # state lock table name - non sensitive
+        key = "FirstVoices/fv-apps/fvapps_terraform.tfstate" # # state bucket's item's name - non sensitive
+        region = "ca-central-1"
     }
 }
 
 provider "aws" {
-  region = ""
+  region = "ca-central-1"
 }

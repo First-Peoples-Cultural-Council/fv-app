723bae5203b4f4d9ffc626554f26652b2638df74
Jonathan Aaron <jon.r.aaron@gmail.com>
2024-02-14 13:14:56 -0700

    Add pagination to the categories api

--- src/services/categoriesApiService.ts
+++ src/services/categoriesApiService.ts
@@ -1,14 +1,24 @@
 import { FvCategory } from '../components/common/data';
-import { fetchData } from './apiService';
+import { fetchPaginatedData } from './apiService';
 import { getCurrentDialect } from '../util/getCurrentDialect';
 
-export const fetchCategoryData = async (): Promise<FvCategory[]> => {
-  return fetchData(
+export const fetchAllData = async (pageSize: number): Promise<FvCategory[]> => {
+  return await fetchPaginatedData(
     `${
       process.env.REACT_APP_BASE_API_URL
     }/sites/${getCurrentDialect()}/categories`,
-    'categories'
+    'categories',
+    pageSize
   );
+};
+
+const PAGE_SIZE = 1000;
+
+export const fetchCategoryData = async (): Promise<FvCategory[]> => {
+  return fetchAllData(PAGE_SIZE).then((result) => {
+    console.log('fetchCategoryData', result);
+    return result;
+  });
 };
 
 export default fetchCategoryData;

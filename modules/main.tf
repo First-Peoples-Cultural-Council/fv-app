940554bafaffec0677e4fe854a11876e2e659634
Rohit Sharma <rsharma@fpcc.ca>
2024-04-05 14:35:45 -0700

    Fine tuning the dynamic bloc conditionals

--- modules/main.tf
+++ modules/main.tf
@@ -3,5 +3,5 @@ locals {
   subdomains         = [for s in var.subdomains : "${s}.${local.local_env_name}${var.domain}"]
   cloudfront_aliases = concat([var.domain], local.subdomains)
   local_env_name     = var.env_name == "prod" ? "" : "${var.env_name}."
-  for_dynamic_blocks = var.env_name == "prod" ? [] : [local.local_env_name]
+  for_dynamic_blocks = var.env_name == "prod" ? [var.env_name] : []
 }

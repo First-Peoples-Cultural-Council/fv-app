7920d8f2b1e850c94d62cbc644024c76b2ef3695
Rohit Sharma <rsharma@fpcc.ca>
2024-04-07 12:12:17 -0700

    Have conditionals for subdomains in the lower environment

--- modules/main.tf
+++ modules/main.tf
@@ -2,7 +2,7 @@ locals {
   has_domain          = var.domain != ""
   local_env_name      = var.env_name == "prod" ? "" : "${var.env_name}."
   domain              = "${local.local_env_name}${var.domain}"
-  subdomains          = [for s in var.subdomains : "${s}${local.domain}"]
+  subdomains          = [for s in var.subdomains : var.env_name == "prod" ? "${s}${local.domain}" :"${s}.${local.domain}" ]
   cloudfront_aliases  = concat([local.domain], local.subdomains)
   application_name    = "${local.local_env_name}${var.application_name}"
   for_dynamic_blocks  = var.env_name == "prod" ? [var.env_name] : []

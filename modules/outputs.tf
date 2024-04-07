9da858e3643dab19f518b9bca359d55500b8451c
Rohit Sharma <rsharma@fpcc.ca>
2024-04-07 16:28:43 -0700

    Move all configs to new repo

--- modules/outputs.tf
+++
@@ -1,4 +0,0 @@
-#Returns the accessible URL of the deployed static website
-output "website-url" {
-  value = local.has_domain ? "https://${local.domain}" : "https://${aws_cloudfront_distribution.this.domain_name}"
-}

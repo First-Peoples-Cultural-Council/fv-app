0d8fff4d84d33e648354b4f2a654d5839a54272b
Cara Barter <carabarter@gmail.com>
2024-05-15 16:24:37 -0700

    cleaning

--- src/components/common/mobile-nav/mobile-nav.tsx
+++ src/components/common/mobile-nav/mobile-nav.tsx
@@ -11,7 +11,6 @@ export interface MobileNavProps {
 
 export function MobileNav({ navItems, extraNavItems, currentTab }: MobileNavProps) {
   const [showExtraNav, setShowExtraNav] = React.useState(false);
-  console.log({navItems})
   return (
     <>
       {/* Mobile Nav Spacer */}

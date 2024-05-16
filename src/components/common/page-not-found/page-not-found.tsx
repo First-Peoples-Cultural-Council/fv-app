d32588a6070f52c45b2d33e9af876d91109ce9f8
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-05-16 09:05:33 -0700

    Update 404 page styling and add useful classes from fv-web

--- src/components/common/page-not-found/page-not-found.tsx
+++ src/components/common/page-not-found/page-not-found.tsx
@@ -1,46 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useButtonStyle } from '../hooks/useButtonStyle';
 
 /* eslint-disable-next-line */
 export interface PageNotFoundProps {}
 
 export function PageNotFound(props: PageNotFoundProps) {
-  const primaryButtonStyle = useButtonStyle('primary');
-  const secondaryButtonStyle = useButtonStyle('primary');
   return (
-    <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-      <div className="mx-auto max-w-max">
-        <main className="sm:flex">
-          <p className="text-4xl font-bold tracking-tight text-color-primary-0 sm:text-5xl">
-            404
-          </p>
-          <div className="sm:ml-6">
-            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                Page not found
-              </h1>
-              <p className="mt-1 text-base text-gray-500">
-                Please check the URL in the address bar and try again.
-              </p>
-            </div>
-            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-              <Link className={primaryButtonStyle} to="/">
-                Go back home
-              </Link>
-              <a
-                href="https://firstvoices.atlassian.net/servicedesk/customer/portal/6"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={secondaryButtonStyle}
-              >
-                Contact support
-              </a>
-            </div>
-          </div>
-        </main>
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-primary">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-fv-charcoal sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-fv-charcoal-light">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link className="btn-contained" to="/">
+            Go back home
+          </Link>
+          <a
+            href="https://firstvoices.atlassian.net/servicedesk/customer/portal/6"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-outlined"
+          >
+            Contact support
+          </a>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 

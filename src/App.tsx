0bd20d329d37219c19b3c3136c1996c366af747d
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-05-27 11:08:11 -0700

    Move nav items into constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,11 @@
+import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { NavigationItem } from './components/common/data';
+
+// FPCC
 import Header from './components/common/header/header';
 import MobileNav from './components/common/mobile-nav/mobile-nav';
-import { Suspense } from 'react';
 import { LoadingSpinner } from './components/common/loading-spinner/loading-spinner';
-
-const navItems: NavigationItem[] = [
-  {
-    id: 'dictionary',
-    label: 'Dictionary',
-    to: '/dictionary',
-    icon: <i className="fv-dictionary" />,
-  },
-  {
-    id: 'learn',
-    label: 'Learn',
-    to: '/learn',
-    icon: <i className="fv-learning" />,
-  },
-  {
-    id: 'bookmarks',
-    label: 'Bookmarks',
-    to: '/bookmarks',
-    icon: <i className="fv-bookmark" />,
-  },
-];
-const extraNavItems: NavigationItem[] = [
-  {
-    id: 'settings',
-    label: 'Settings',
-    to: '/settings',
-    icon: <i className="fv-cog" />,
-  },
-  {
-    id: 'about',
-    label: 'About',
-    to: '/about',
-    icon: <i className="fv-info" />,
-  },
-];
+import { navItems, extraNavItems } from './constants/navigation';
 
 export function App() {
   let currentTab = 'dictionary';
--- src/components/learn/learn.tsx
+++ src/components/learn/learn.tsx
@@ -1,67 +1,21 @@
-import { matchRoutes, Outlet, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { matchRoutes, Outlet, useLocation } from 'react-router-dom';
+
+// FPCC
 import SubNavDesktop from '../sub-nav-desktop/sub-nav-desktop';
 import SubNavMobile from '../sub-nav-mobile/sub-nav-mobile';
-import { SubNavItem } from '../common/data';
 import PageHeader from '../common/page-header/page-header';
-
-const navItems: SubNavItem[] = [
-  {
-    id: 'stories',
-    path: 'stories',
-    icon: 'fv-stories',
-    iconSize: 'text-3xl',
-    title: 'Stories',
-    colors: {
-      to: 'to-color-stories-light',
-      from: 'from-color-stories-dark',
-      hoverText: 'hover:text-color-stories-dark',
-      activeText: 'text-color-stories-dark',
-      border: 'border-color-stories-dark',
-    },
-    activePathMatches: [{ path: 'learn/stories' }, { path: 'learn' }], // Stories is the default page for 'learn'
-  },
-  {
-    id: 'songs',
-    path: 'songs',
-    icon: 'fv-songs',
-    iconSize: 'text-4xl',
-    title: 'Songs',
-    colors: {
-      to: 'to-color-songs-light',
-      from: 'from-color-songs-dark',
-      hoverText: 'hover:text-color-songs-dark',
-      activeText: 'text-color-songs-dark',
-      border: 'border-color-songs-dark',
-    },
-    activePathMatches: [{ path: 'learn/songs' }],
-  },
-  {
-    id: 'flashcards',
-    path: 'flashcards',
-    icon: 'fv-flashcard',
-    iconSize: 'text-4xl',
-    title: 'Flashcards',
-    colors: {
-      to: 'to-color-flashcards-light',
-      from: 'from-color-flashcards-dark',
-      hoverText: 'hover:text-color-flashcards-dark',
-      activeText: 'text-color-flashcards-dark',
-      border: 'border-color-flashcards-dark',
-    },
-    activePathMatches: [{ path: 'learn/flashcards' }],
-  },
-];
+import { learnSubNavItems } from '../../constants/navigation';
 
 /* eslint-disable-next-line */
 export interface LearnViewProps {}
 
 export function LearnView(props: LearnViewProps) {
   const location = useLocation();
-  const [currentNavItem, setCurrentNavItem] = useState(navItems[0]);
+  const [currentNavItem, setCurrentNavItem] = useState(learnSubNavItems[0]);
 
   useEffect(() => {
-    const currentNavItem = navItems.find((item) =>
+    const currentNavItem = learnSubNavItems.find((item) =>
       matchRoutes([{ path: `/learn/${item.path}` }], location)
     );
     if (currentNavItem) {
@@ -71,7 +25,7 @@ export function LearnView(props: LearnViewProps) {
 
   return (
     <div>
-      <SubNavMobile navItems={navItems} />
+      <SubNavMobile navItems={learnSubNavItems} />
       {currentNavItem && (
         <PageHeader
           title={currentNavItem.title}
@@ -82,7 +36,7 @@ export function LearnView(props: LearnViewProps) {
         />
       )}
       <div className="flex w-full">
-        <SubNavDesktop navItems={navItems} />
+        <SubNavDesktop navItems={learnSubNavItems} />
         {currentNavItem && <Outlet />}
       </div>
     </div>

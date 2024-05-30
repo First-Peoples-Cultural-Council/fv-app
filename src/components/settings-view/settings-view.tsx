7f83a4e89db96e7c608ee39288e5bb7845ae9f9b
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-05-30 11:27:33 -0700

    Quiet warning

--- src/components/settings-view/settings-view.tsx
+++ src/components/settings-view/settings-view.tsx
@@ -7,7 +7,6 @@ import ConfirmDialog from '../common/confirm/confirm';
 export interface SettingsViewProps {}
 
 export function SettingsView(props: SettingsViewProps) {
-
   const [db, setDb] = useState<IndexedDBService>();
   const [mediaCount, setMediaCount] = useState(0);
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
@@ -17,25 +16,36 @@ export function SettingsView(props: SettingsViewProps) {
   }, []);
 
   useEffect(() => {
-    if(db){
+    if (db) {
       updateMediaCount();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [db]);
 
   return (
     <>
       <div className={styles['container']}>
         <div className="m-4">
-          <p className="mb-3">Media files are stored in a local cache, so they are available when you are offline. You can clear this cache to free up space on your device, but the files will stop being available offline until you download them again.</p>
-          <p className="mb-6">Currently you have <span id="localMediaCount">{mediaCount}</span> files in your local cache.</p>
+          <p className="mb-3">
+            Media files are stored in a local cache, so they are available when
+            you are offline. You can clear this cache to free up space on your
+            device, but the files will stop being available offline until you
+            download them again.
+          </p>
+          <p className="mb-6">
+            Currently you have <span id="localMediaCount">{mediaCount}</span>{' '}
+            files in your local cache.
+          </p>
 
-          <p><button
-            className="btn-contained bg-secondary"
-            onClick={() => setShowConfirmDialog(true)}
-          >
-            Clear Media Cache
-            {/* <i className=""></i> */}
-          </button></p>
+          <p>
+            <button
+              className="btn-contained bg-secondary"
+              onClick={() => setShowConfirmDialog(true)}
+            >
+              Clear Media Cache
+              {/* <i className=""></i> */}
+            </button>
+          </p>
         </div>
       </div>
       {showConfirmDialog && (

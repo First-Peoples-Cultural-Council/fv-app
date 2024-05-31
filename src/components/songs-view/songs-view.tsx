28dff5c4d15cace6186446b8af9c5a8176c37602
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-05-31 16:25:55 -0700

    Add direct route for song bookmarks and mobile view

--- src/components/songs-view/songs-view.tsx
+++ src/components/songs-view/songs-view.tsx
@@ -1,117 +1,75 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
-import { useLocation } from 'react-router';
+import { useNavigate } from 'react-router';
 
 // FPCC
-import FullScreenModal from '../common/full-screen-modal/full-screen-modal';
 import { FVSong } from '../common/data/types';
-import { useModal } from '../common/use-modal/use-modal';
-import fetchSongsData from '../../services/songsApiService';
 import FvImage from '../common/image/image';
-import { LoadingSpinner } from '../common/loading-spinner/loading-spinner';
 import SongView from '../song-view/song-view';
 
-/* eslint-disable-next-line */
-export interface SongsViewProps {}
+export interface SongsViewProps {
+  songsData: FVSong[];
+}
 
-export function SongsView(props: SongsViewProps) {
-  const location = useLocation();
-  const { setShowModal, showModal, closeModal } = useModal();
-  const [songsData, setSongsData] = useState<FVSong[]>([]);
+export function SongsView({ songsData }: SongsViewProps) {
+  const navigate = useNavigate();
   const [selectedSong, setSelectedSong] = useState<FVSong | null>(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchDataAsync = async () => {
-      try {
-        const result = await fetchSongsData();
-        setSongsData(result);
-        setLoading(false);
-      } catch (error) {
-        // Handle error scenarios
-      }
-    };
-
-    fetchDataAsync();
-  }, []);
-
-  useEffect(() => {
-    const songId = location.hash.slice(1).split('?')[0];
-    const song = songsData.find((song) => song.id === songId);
-    if (song) {
-      setSelectedSong(song);
-      if (window.matchMedia('(max-width: 1024px').matches) {
-        setShowModal(true);
-      }
+  const onSongClick = (song: FVSong) => {
+    setSelectedSong(song);
+    if (window.matchMedia('(max-width: 768px').matches) {
+      navigate(`/learn/songs/${song?.id}`);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [songsData, location]);
+  };
 
   return (
-    <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-full">
-        <div className="overflow-y-auto max-h-calc-185 md:max-h-calc-125">
-          {loading && <LoadingSpinner />}
-          {!loading &&
-            songsData.map((song: FVSong) => {
-              return (
-                <button
-                  key={song.id}
-                  className={classNames(
-                    'block rounded-lg bg-white p-6 m-2 shadow-lg hover:bg-slate-100 cursor-pointer',
-                    {
-                      'lg:bg-green-100 lg:hover:bg-green-200':
-                        song.id === selectedSong?.id,
-                    }
-                  )}
-                  onClick={() => {
-                    setSelectedSong(song);
-                    if (window.matchMedia('(max-width: 1024px').matches) {
-                      setShowModal(true);
-                    }
-                  }}
-                >
-                  <div className="grid grid-cols-10 gap-4">
-                    <div className="col-span-2 flex">
-                      {song?.relatedImages.length === 0 && (
-                        <div className="fv-songs text-5xl self-center border border-solid" />
-                      )}
-                      {song?.relatedImages.length !== 0 && (
-                        <FvImage
-                          disabledClassName="text-6xl"
-                          src={song?.relatedImages[0]?.thumbnail?.path ?? ''}
-                          alt={song?.relatedImages[0].title}
-                        />
-                      )}
-                    </div>
-                    <div className="col-span-6">
-                      <div>
-                        <h1 className="font-bold">{song.title}</h1>
-                      </div>
-                      <h1 className="truncate">{song.titleTranslation}</h1>
-                    </div>
-                    <div className="self-center col-span-1"></div>
-                    <div className="place-self-end self-center">
-                      <i className="fv-right-open" />
+    <div className="w-full bg-gray-100">
+      <div className="grid grid-cols-2 w-full h-full max-h-calc-185 md:max-h-calc-125">
+        <div className="col-span-2 md:col-span-1 overflow-y-auto md:space-y-2 md:p-2">
+          {songsData?.map((song: FVSong) => {
+            return (
+              <button
+                key={song.id}
+                className={classNames(
+                  'w-full border border-gray-200 md:rounded-lg bg-white p-4 shadow-lg hover:bg-slate-100 cursor-pointer',
+                  {
+                    'md:bg-green-100 md:hover:bg-green-200':
+                      song.id === selectedSong?.id,
+                  }
+                )}
+                onClick={() => onSongClick(song)}
+              >
+                <div className="grid grid-cols-8 gap-2">
+                  <div className="col-span-2">
+                    {song?.relatedImages.length > 0 ? (
+                      <FvImage
+                        disabledClassName="text-6xl"
+                        src={song?.relatedImages[0]?.thumbnail?.path ?? ''}
+                        alt={song?.relatedImages[0].title}
+                      />
+                    ) : (
+                      <div className="fv-songs text-5xl self-center border border-solid" />
+                    )}
+                  </div>
+                  <div className="col-span-5 flex items-center justify-center">
+                    <div>
+                      <div className="font-bold">{song.title}</div>
+                      <div className="truncate">{song.titleTranslation}</div>
                     </div>
                   </div>
-                </button>
-              );
-            })}
+                  <div className="col-span-1 flex items-center justify-end">
+                    <i className="fv-right-open" />
+                  </div>
+                </div>
+              </button>
+            );
+          })}
         </div>
-        <div
-          className="hidden lg:block lg:overflow-y-auto"
-          style={{ height: 'calc(100vh - 150px)' }}
-        >
+        <div className="hidden bg-white md:col-span-1 md:block md:overflow-y-auto">
           {selectedSong && <SongView song={selectedSong} />}
         </div>
       </div>
-      {showModal && selectedSong && (
-        <FullScreenModal onClose={() => closeModal()}>
-          <SongView song={selectedSong} />
-        </FullScreenModal>
-      )}
-    </>
+    </div>
   );
 }
 

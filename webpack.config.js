c9a2d0f14187573667138a1825fb9af4f33ad6f7
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-06-03 14:03:52 -0700

    updated webpack.config.js for dev environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,126 +1,97 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-const path = require("path");
-const webpack = require('webpack');
-const dotenv = require('dotenv').config( {
-  path: path.join(__dirname, '.env')
-} );
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const WebpackAssetsManifest = require("webpack-assets-manifest");
+const WebpackAssetsManifest = require('webpack-assets-manifest');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
-const stylesHandler = MiniCssExtractPlugin.loader;
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW({
-  clientsClaim: true,
-  skipWaiting: false,
-  swDest: 'service-worker.js',
-  sourcemap: true,}));
-  } else {
-    config.mode = "development";
-    config.devtool = "source-map";
-    config.devServer = {
-    static: {
-      directory: path.resolve(__dirname, "build"),
-    },
-    open: true,
-    host: "localhost",
-    port: 3000
-    };
-  }
-  return config;
-};
+const root = path.resolve(__dirname);
+const buildPath = path.resolve(root, 'build');
 
 const config = {
   entry: {
-    main: ['./src/index.tsx', './src/index.css'],
-    serviceWorker: ['./src/service-worker.ts'],
+    main: '/src/index.tsx',
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: buildPath,
+    publicPath: buildPath,
     clean: true,
-    filename: "[name].js",
-    cssFilename: "[name].[contenthash].css",
-    chunkFilename: "main.[contenthash]-chunk.js",
-    assetModuleFilename: '[path][name].[hash][ext][query]'
+    filename: '[name].js',
+    cssFilename: '[name].[contenthash].css',
+    chunkFilename: 'main.[contenthash]-chunk.js',
+    assetModuleFilename: '[path][name].[hash][ext][query]',
   },
-
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-
-    new MiniCssExtractPlugin(),
-
-    new webpack.DefinePlugin( {
-      "process.env": dotenv.parsed
-    } ),
-
     new CopyPlugin({
-    patterns: [
-      {
-        from: "./public/",
-        globOptions: {
-          ignore: [
-            // Ignore all `html` files
-            "**/*.html",
-          ]}}],
+      patterns: [
+        {
+          from: 'public',
+          to: 'build',
+          globOptions: {
+            ignore: [
+              // Ignore all `html` files
+              '**/*.html',
+            ],
+          },
+        },
+      ],
     }),
-     new WebpackAssetsManifest({
-      entrypoints:true
+    new WebpackAssetsManifest({
+      entrypoints: true,
     }),
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true
-            }
-          }],
-        include: /\.module\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
-    {
-      test: /\.css$/,
-      use: [stylesHandler, "css-loader"],
-      exclude: /\.module\.css$/,
-    },
-    {
-      test: /\.svg$/,
-      use: "file-loader",
-    },
-    {
-      test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-      type: "asset/resource",
-      generator: {
-        filename: 'static/media/[name].[hash][ext][query]'
-      }
-    },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|json)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+    config.plugins.push(
+      new WorkboxWebpackPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: false,
+        swDest: 'service-worker.js',
+        sourcemap: true,
+      })
+    );
+  } else {
+    config.mode = 'development';
+    config.devtool = 'inline-source-map';
+    config.devServer = {
+      static: {
+        directory: buildPath,
+      },
+      server: 'https',
+      open: true,
+      host: '0.0.0.0',
+      port: 3000,
+    };
+    config.performance = {
+      hints: 'warning',
+    };
+  }
+  return config;
 };

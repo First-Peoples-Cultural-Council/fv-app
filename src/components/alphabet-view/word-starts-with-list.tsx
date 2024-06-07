826ce6d1745cea5cb075a1e8ff460e90bdb66bcd
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-06-07 10:54:49 -0700

    Extract hook for filtering dictionary by startsWithChar

--- src/components/alphabet-view/word-starts-with-list.tsx
+++ src/components/alphabet-view/word-starts-with-list.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 // FPCC
 import WordAlphabetRowCard from './word-row-card';
 import { FvCharacter, FvWord } from '../common/data';
 import { useDictionaryData } from '../dictionary-page/dictionary-page';
+import { useStartsWithChar } from '../../util/useStartsWithChar';
 
 export interface WordStartsWithListProps {
   selected: FvCharacter;
@@ -13,16 +14,7 @@ export function WordStartsWithList({
   selected,
 }: Readonly<WordStartsWithListProps>) {
   const { dictionaryData } = useDictionaryData();
-  const [entriesStartingWith, setEntriesStartingWith] = useState<FvWord[]>([]);
-
-  useEffect(() => {
-    if (dictionaryData && selected.sortingFormNum !== undefined) {
-      const entries = [...dictionaryData].filter((entry) => {
-        return entry.sorting_form[0] === selected.sortingFormNum;
-      });
-      setEntriesStartingWith(entries);
-    }
-  }, [dictionaryData, selected.sortingFormNum]);
+  const { entriesStartingWith } = useStartsWithChar(dictionaryData, selected);
 
   return (
     <div className="w-full">
@@ -31,13 +23,13 @@ export function WordStartsWithList({
         <span className="text-4xl bold">{selected?.title}</span>
       </div>
       <div className="space-y-2 md:px-2">
-        {entriesStartingWith?.map((term: FvWord) => {
+        {entriesStartingWith?.map((entry: FvWord) => {
           return (
             <div
-              key={`${term.source}-${term.entryID}`}
+              key={`${entry.source}-${entry.entryID}`}
               className="flex w-full col-span-1"
             >
-              <WordAlphabetRowCard term={term} />
+              <WordAlphabetRowCard term={entry} />
             </div>
           );
         })}

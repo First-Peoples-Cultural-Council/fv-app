7cc30e17dc3b9608dd484abd0e51e731f85686c4
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2024-06-10 10:44:04 -0600

    FW-5749 App Install Prompt (#162)
    * add very rough install prompt prototype
    
    * further styling, use correct logos, move modal to app component
    
    * move logic out of App component
    
    * add logic for closing and app installation flow
    
    * sonar fixes
    
    * sonar fixes
    
    * styling changes
    
    * undo unnecessary dictionary page change
    
    * file structure changes for install prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 // FPCC
@@ -6,12 +6,14 @@ import Header from './components/common/header/header';
 import MobileNav from './components/common/mobile-nav/mobile-nav';
 import { LoadingSpinner } from './components/common/loading-spinner/loading-spinner';
 import { navItems, extraNavItems } from './constants/navigation';
+import InstallPrompt from './components/install-prompt/install-prompt';
 
 export function App() {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <Header navItems={navItems} extraNavItems={extraNavItems} />
       <Outlet />
+      <InstallPrompt />
       <MobileNav navItems={navItems} extraNavItems={extraNavItems} />
     </Suspense>
   );

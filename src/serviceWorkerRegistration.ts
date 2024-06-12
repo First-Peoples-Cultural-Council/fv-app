0a8836c7cee5d29b1f10ec0bb1a396324e81c666
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-06-12 09:59:58 -0700

    Added serviceWorkers to webpack.config

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -28,7 +28,7 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = 'serviceWorker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.

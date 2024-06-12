a538a32dd20c6c30554a0c7de7f98245a4711106
Bronson Chapman <bronsonchapman01@gmail.com>
2024-06-12 13:29:18 -0600

    also sort dictionary list by custom order

--- src/util/useStartsWithChar.ts
+++ src/util/useStartsWithChar.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 // FPCC
 import { FvCharacter, FvWord } from '../components/common/data';
+import sortByCustomOrder from './sortByCustomOrder';
 
 export const useStartsWithChar = (
   dictionaryData: FvWord[],
@@ -13,13 +14,7 @@ export const useStartsWithChar = (
       const entries = [...dictionaryData].filter((entry) => {
         return entry.sorting_form[0] === character.sortingFormNum;
       }).sort((a, b) => {
-        const len = Math.min(a.sorting_form.length, b.sorting_form.length);
-        for (let i = 0; i < len; i++) {
-          if (a.sorting_form[i] !== b.sorting_form[i]) {
-            return a.sorting_form[i] - b.sorting_form[i];
-          }
-        }
-        return a.sorting_form.length - b.sorting_form.length;
+        return sortByCustomOrder(a, b);
       });
       setEntriesStartingWith(entries);
     }

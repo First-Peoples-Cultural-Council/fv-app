1644a961b219269cd9093e5c199f53b5b1fd6eb2
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2024-06-12 17:02:54 -0600

    FW-5885 Use custom sort order in dictionary lists (#177)
    * fix alphabet list custom sort order
    
    * also sort dictionary list by custom order

--- src/util/useStartsWithChar.ts
+++ src/util/useStartsWithChar.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 // FPCC
 import { FvCharacter, FvWord } from '../components/common/data';
+import sortByCustomOrder from './sortByCustomOrder';
 
 export const useStartsWithChar = (
   dictionaryData: FvWord[],
@@ -12,6 +13,8 @@ export const useStartsWithChar = (
     if (dictionaryData && character.sortingFormNum !== undefined) {
       const entries = [...dictionaryData].filter((entry) => {
         return entry.sorting_form[0] === character.sortingFormNum;
+      }).sort((a, b) => {
+        return sortByCustomOrder(a, b);
       });
       setEntriesStartingWith(entries);
     }

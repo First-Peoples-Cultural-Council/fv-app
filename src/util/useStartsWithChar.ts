5f07fe3923daf5929ce44172b1a08fc5ea9c23f9
Bronson Chapman <bronsonchapman01@gmail.com>
2024-06-12 11:07:29 -0600

    fix alphabet list custom sort order

--- src/util/useStartsWithChar.ts
+++ src/util/useStartsWithChar.ts
@@ -12,6 +12,14 @@ export const useStartsWithChar = (
     if (dictionaryData && character.sortingFormNum !== undefined) {
       const entries = [...dictionaryData].filter((entry) => {
         return entry.sorting_form[0] === character.sortingFormNum;
+      }).sort((a, b) => {
+        const len = Math.min(a.sorting_form.length, b.sorting_form.length);
+        for (let i = 0; i < len; i++) {
+          if (a.sorting_form[i] !== b.sorting_form[i]) {
+            return a.sorting_form[i] - b.sorting_form[i];
+          }
+        }
+        return a.sorting_form.length - b.sorting_form.length;
       });
       setEntriesStartingWith(entries);
     }

57f84e889f2d383828de17a044000ea9b552b398
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2024-06-13 09:56:47 -0700

    Fw-5884, prevent broken audio button when switching from offline to online (#176)
    * fw-5884, load audio when internet is available again
    
    * remove extra log
    
    * remove log

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -197,7 +197,6 @@ async function getFileFromResponse(response: Response, filename: string): Promis
   const blob = await response.blob();
 
   if(blob) {
-    console.log("Making blob into file: ", filename, blob.type, blob, response);
     return new File([blob], filename);
   }
 

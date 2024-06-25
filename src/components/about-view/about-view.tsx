0532c78aed7174a8fc34a56d40a45b4c85bd6527
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-06-25 16:24:29 -0700

    FW-5913 app about page update (#181)
    * Add site specific details to AboutView
    
    * Make use of AboutView in InstallPrompt
    
    * Fix issue where extraNav menu was covered by search input
    
    * Mark props as read only

--- src/components/about-view/about-view.tsx
+++ src/components/about-view/about-view.tsx
@@ -1,28 +1,71 @@
-/* eslint-disable-next-line */
-export interface AboutViewProps {}
+import React, { ReactNode } from 'react';
 
-export function AboutView(props: AboutViewProps) {
+// FPCC
+import fpccLogo from '../../assets/images/fpccLogoColorWhite.svg';
+import fpcfLogo from '../../assets/images/fpcfLogoWhite.svg';
+import useSiteTitleFromManifest from '../../util/useSiteTitleFromManifest';
+
+export interface AboutViewProps {
+  children?: ReactNode;
+}
+
+export function AboutView({ children }: Readonly<AboutViewProps>) {
+  // Site information from manifest and hostname
+  const hostnameParts = window.location.hostname.split('.');
+  const subdomain = hostnameParts[0];
+  const manifestUrl = `${process.env.PUBLIC_URL}/manifest.${subdomain}.json`;
+  const siteTitle = useSiteTitleFromManifest(manifestUrl);
+  const siteURL = `https://www.firstvoices.com/${subdomain}/`;
+  const logoURL = `${process.env.PUBLIC_URL}/${subdomain}/logo192.png`;
+
   return (
-    <div className="p-4">
-      <h1 className="text-3xl">About this App</h1>
-      <br />
-      <h1 className="text-xl">
-        This app is called{' '}
-        <span className="text-secondary-500">
-          {process.env.REACT_APP_ABOUT_APP_NAME}
-        </span>{' '}
-        and is provided by{' '}
-        <span className="text-secondary-500">
-          {process.env.REACT_APP_ABOUT_ORG_NAME}
-        </span>
-      </h1>
-      <br />
-      <h1 className="text-xl">
-        The current version of this app is{' '}
-        {process.env.REACT_APP_VERSION_NUMBER}
-      </h1>
-      <br />
-      <h1 className="text-xl">{process.env.REACT_APP_ABOUT_ADDITIONAL_INFO}</h1>
+    <div data-testid="AboutView" className="max-w-3xl mx-auto">
+      <main className="p-4 sm:p-6 md:p-6 lg:p-8 text-center space-y-4">
+        <div className="space-y-4">
+          <h1 className="w-full text-center text-3xl mb-4">
+            {siteTitle + ' App'}
+          </h1>
+          <img src={logoURL} alt="App logo" className="w-24 h-24 mx-auto" />
+          <p className="text-lg">
+            Browse words and phrases in the dictionary, practice with
+            flashcards, bookmark content and more.
+          </p>
+        </div>
+        {children}
+        <div className="space-y-2">
+          <p className="text-sm">
+            Powered by the{' '}
+            <a href={siteURL} className="text-blue-500 underline">
+              {siteTitle} FirstVoices language site.
+            </a>
+          </p>
+          <p className="text-sm">
+            Compatible with iPhone, iPad, Android, Chromebook, Windows, and
+            more.
+          </p>
+        </div>
+      </main>
+      <footer className="bg-charcoal-500 p-4">
+        <p className="text-white text-center text-sm">An initiative of:</p>
+        <div className="flex justify-center flex-wrap md:space-x-5 my-2">
+          <a href="https://www.fpcc.ca/" target="_blank" rel="noreferrer">
+            <img
+              className="h-14"
+              src={fpccLogo}
+              alt="First People's Cultural Council Logo"
+              loading="lazy"
+            />
+          </a>
+          <a href="https://www.fpcf.ca/" target="_blank" rel="noreferrer">
+            <img
+              className="h-14"
+              src={fpcfLogo}
+              alt="First People's Cultural Foundation Logo"
+              loading="lazy"
+            />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }

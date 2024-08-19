557a6fe02c33feab569890f30c869ea88893b199
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-19 16:20:50 -0700

    Remove Playwright

--- tests/alphabet.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has sidebar', async ({ page }) => {
-  await page.goto('/alphabet');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(page.locator('nav.sidebar')).toHaveAttribute('role', 'complementary');
-});
--- tests/categories.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Should load categories route', async ({ page }) => {
-  await page.goto('/categories');
-  await page.waitForSelector('text=Categories');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/First Voices/i);
-});

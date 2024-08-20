7a134378a777a063a99d934c99082e5d2ed4e076
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-20 09:38:04 -0700

    Update jest environment to fix run error

--- src/components/common/alert/alert.spec.tsx
+++ src/components/common/alert/alert.spec.tsx
@@ -1,17 +1,21 @@
-import { render } from "@testing-library/react";
+/**
+ * @jest-environment jsdom
+ */
 
-import Alert from "./alert";
+import { render } from '@testing-library/react';
 
-describe("Alert", () => {
-  it("should render successfully", () => {
+import Alert from './alert';
+
+describe('Alert', () => {
+  it('should render successfully', () => {
     const { baseElement } = render(
       <Alert
-        type={"error"}
+        type={'error'}
         message={undefined}
         showDismissButton={false}
         showAlert={false}
         dismissAlert={function (): void {
-          throw new Error("Function not implemented.");
+          throw new Error('Function not implemented.');
         }}
       />
     );

1a3b3e2793cf2f855ee9d0597260d1d83c0c6916
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-21 12:30:33 -0700

    Add jsx-runtime config to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -10,5 +10,6 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   eslintPluginPrettierRecommended,
-];
+]

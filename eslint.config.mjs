a003b7c4f51dc76998c65201141b67dbb4a9d2bb
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-22 10:00:03 -0700

    Adjust eslint config and add ignores and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,34 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import babelParser from '@babel/eslint-parser'
+import typescriptParser from '@typescript-eslint/parser'
+
+import { fixupPluginRules } from '@eslint/compat'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 
 export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: [
+      'node_modules/',
+      'build/',
+      'src/assets/',
+      '__mocks__/',
+      'public/mtd-ui.min.js',
+      'config-overrides.js',
+      '*.config.js',
+    ],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat['jsx-runtime'],
+  eslintPluginPrettierRecommended,
+  {
     languageOptions: {
       globals: globals.browser,
-      parser: babelParser,
+      parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: {
@@ -29,13 +47,16 @@ export default [
         version: 'detect',
       },
     },
-    plugins: [
-      pluginJs.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintPluginReact.configs.flat.recommended,
-      eslintPluginReact.configs.flat['jsx-runtime'],
-      eslintPluginReactHooks,
-      eslintPluginPrettierRecommended,
-    ],
+    plugins: {
+      'react-hooks': fixupPluginRules(eslintPluginReactHooks),
+    },
+    rules: {
+      'consistent-return': ['warn'],
+      'no-underscore-dangle': ['off'],
+      'no-unsafe-optional-chaining': ['warn'],
+      '@typescript-eslint/no-explicit-any': ['warn'],
+      '@typescript-eslint/no-unsafe-function-type': ['warn'],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
   },
 ]

adf90372c227108b820268d53410d970c4278d59
Sahil Sharma <sahil.sharma071997@gmail.com>
2026-06-25 14:30:24 -0700

    FW-7083: Added data-testIds and fixed sizing of the download media button (#528)
    * Added data-testIds and fixed sizing of the download media button
    
    * Updated styling for the download related media button in alphabet view

--- src/components/alphabet-view/download-button.tsx
+++ src/components/alphabet-view/download-button.tsx
@@ -84,7 +84,10 @@ export function DownloadButton({ dictionaryData, selected }: Readonly<DownloadBu
       <div className="flex justify-center items-center m-4">
         <button
           disabled={buttonDisabled}
-          className={buttonDisabled ? 'btn-contained bg-gray-500' : 'btn-contained bg-primary-300 cursor-pointer'}
+          className={
+            (buttonDisabled ? 'bg-gray-500 ' : 'bg-primary-300 cursor-pointer') +
+            ' btn-contained w-full max-w-xs transition-colors duration-300'
+          }
           onClick={() =>
             isOnline
               ? promptForDownload()
@@ -94,9 +97,17 @@ export function DownloadButton({ dictionaryData, selected }: Readonly<DownloadBu
                 })
           }
         >
-          <p className="text-xl">
-            {getButtonLabel()} <span className="fv-cloud-arrow-down-regular justify-self-end" />
-          </p>
+          <div className="flex flex-col items-center justify-center space-y-2">
+            <div className="relative w-[240px] h-6 overflow-hidden">
+              <p
+                key={getButtonLabel()}
+                className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-100"
+              >
+                {getButtonLabel()}
+                <span className="fv-cloud-arrow-down-regular text-2xl" />
+              </p>
+            </div>
+          </div>
         </button>
       </div>
       {showConfirmDialog && (

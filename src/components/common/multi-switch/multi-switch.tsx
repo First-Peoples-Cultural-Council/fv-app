7dd4e90bad6d83b2706aef2e3e31c6e49b308b5e
Guy McAuliffe <guy@fpcc.ca>
2026-07-27 11:09:05 -0700

    Ensure multi-switch remains on single row

--- src/components/common/multi-switch/multi-switch.tsx
+++ src/components/common/multi-switch/multi-switch.tsx
@@ -15,19 +15,22 @@ export function MultiSwitch({ selected: initialSelected, items, onToggle }: Read
   const [selected, setSelected] = useState<number>(initialSelected)
 
   return (
-    <div className={'multi-switch p-2'}>
-      <div className={'w-fit rounded-lg'}>
+    <div id="MultiSwitch" className="p-2">
+      <div className="w-fit rounded-lg inline-flex">
         {items.map((item, index) => {
           const ItemIcon = item?.icon
           return (
             <button
               key={item.name}
-              className={classNames('py-2 px-3 border-solid border-gray-300 border shadow-lg text-center space-x-1', {
-                'bg-primary-500 text-white hover:bg-primary-400 border-primary': selected === index,
-                'bg-white text-primary-500 hover:bg-gray-100': selected !== index,
-                'rounded-l-lg': index === 0,
-                'rounded-r-lg': index === items.length - 1,
-              })}
+              className={classNames(
+                'inline-flex items-center justify-center py-2 px-3 border-solid border-gray-300 border shadow-lg space-x-1',
+                {
+                  'bg-primary-500 text-white hover:bg-primary-400 border-primary': selected === index,
+                  'bg-white text-primary-500 hover:bg-gray-100': selected !== index,
+                  'rounded-l-lg': index === 0,
+                  'rounded-r-lg': index === items.length - 1,
+                }
+              )}
               onClick={() => {
                 setSelected(index)
                 onToggle(index)

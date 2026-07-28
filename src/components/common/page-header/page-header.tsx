9469aac2393bdc1eca0f2e6d61982adb1e4b095a
Guy McAuliffe <guy@fpcc.ca>
2026-07-28 10:10:51 -0700

    Minor styling adjustments

--- src/components/common/page-header/page-header.tsx
+++ src/components/common/page-header/page-header.tsx
@@ -10,7 +10,7 @@ export function PageHeader({ title, backgroundColors, children }: Readonly<PageH
   return (
     <header
       role="banner"
-      className={`sub-header flex py-5 px-4 bg-gradient-to-t ${backgroundColors.from} ${backgroundColors.to} justify-between items-center`}
+      className={`sub-header flex h-16 py-5 px-4 bg-gradient-to-t ${backgroundColors.from} ${backgroundColors.to} justify-between items-center`}
     >
       <div className="text-white uppercase mr-2">{title}</div>
       {children}
